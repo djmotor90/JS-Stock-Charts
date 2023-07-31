809225aaf35b4250156bc742de23d651138894b6
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-07-30 23:25:58 -0500

    Done

--- public/index.js
+++ public/index.js
@@ -1,9 +1,75 @@
 async function main() {
-
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+    
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // Line Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.slice().reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.slice().reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
+    // Highest Price Bar Chart
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest Price',
+                data: stocks.map(stock => getHighestPrice(stock.values)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol)),
+            }]
+        }
+    });
+
+    // Average Price Pie Chart
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Average Price',
+                data: stocks.map(stock => getAveragePrice(stock.values)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+            }]
+        }
+    });
+
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+
+    function getHighestPrice(values){
+        return Math.max(...values.map(value => parseFloat(value.high)));
+    }
+
+    function getAveragePrice(values){
+        const sum = values.reduce((total, value) => total + parseFloat(value.high), 0);
+        return sum / values.length;
+    }
 }
 
 main()
